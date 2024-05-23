d3f9046917983f546929c254ee77f6eb0e885760
CHARITY-WACHIRA <kouwywachira@gmail.com>
2024-05-23 15:40:57 +0300

    completed assignment

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -3,5 +3,9 @@
 import ipdb
 
 def plus_two(num):
-    num + 2
-    return num
+    result = num + 2  # Assign the result of num + 2 to a variable
+    return result  # Return the result
+
+# Example usage
+output = plus_two(3)
+print(output)  # Output: 5
